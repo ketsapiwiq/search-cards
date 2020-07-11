da2d8d1947840df7b3ea932ec8317840c5f99d69
Hadrien <26697460+ketsapiwiq@users.noreply.github.com>
2020-07-11 11:45:34 +0200

    wip

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 import json
+import logging
 
 # TODO: json to dict
 
@@ -8,50 +9,43 @@ class JsonEntry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     json = db.Column(db.String(), index=False, unique=False)
 
-
-# def json_entry(func):
-#     def wrapper():
-#         # ???
-#         # self.entry.json  = json.dumps(data)
-#         func()
-#     return wrapper
-
-
-class Card:
+class Card(dict):
     def __init__(self, id=None, data=None):
         if id:
-            self.entry = JsonEntry.query.get(id)
-            self.data = json.loads(self.entry.json)
+            entry = json.loads(JsonEntry.query.get(id).json)
+            self = {'id':id, **entry.json}
             if data:
                 self.update(data)
         else:
-            self.entry = JsonEntry()
-            self.entry.json = json.dumps(data)
-            self.data = data
-        self.id = self.entry.id
+            entry = JsonEntry()
+            entry.json = json.dumps(data)
+            self = {'id':entry.id, **entry.json}
+        return
 
-    # should we use a decorator?
-    # @json_entry
     def update(self, data):
-        self.data.__dict__.update(data)
-        self.entry.json = json.dumps(self.data)
+        self.__dict__.update(data)
 
     def save(self):
-        db.session.add(self.entry)
+        entry = JsonEntry.query.get(self['id'])
+        entry.json = json.dumps(self.__dict__)
+        # entry.json = json.dumps(self)
+        db.session.add(entry)
         db.session.commit()
 
     @classmethod
     def get(cls, id):
-        return Card(id=id)
+        return {'id':id, **JsonEntry.query.get(id=id).json}
 
     @classmethod
     def query(cls, expression):
         results = []
-
-        for item in JsonEntry.query.all():
+        all_entries = JsonEntry.query.all()
+        for item in all_entries:
             data = json.loads(item.json)
             for value in data.values():
                 if expression in value:
-                    results.append(cls.get(item.id))
+                    # results.append(item.json))
+                    results.append(data)
+                    # results.append(cls.get(item.id))
 
         return results
